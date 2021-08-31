e1ab9d09526925714353a21d56d5d2efcec90660
Rohit <rohit.george@atree.org>
2021-08-31 12:36:03 +0530

    added butterflt_count.index view

---
+++ resources/views/butterfly_count/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid h4">
+	<div class="d-flex justify-content-center">
+		<button type="button" id="upload-form" class="btn btn-block btn-success w-50" data-bs-toggle="modal" data-bs-target="#importModal">
+			Upload a Form
+		</button>
+	</div>
+	<div class="h1">{{$form_nos}} Forms Submitted</div>
+
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="importModal" tabindex="-1" aria-labelledby="ImportButterflyCountXLSX" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="ImportButterflyCountXLSX">Import a completed Butterfly Count Sheet</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<form method="POST" action="/butterfly_count/import" enctype="multipart/form-data">
+				@csrf
+				<div class="modal-body">
+					<div class="mb-3">
+						<input class="form-control" type="file" name="count_file" required>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save changes</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
