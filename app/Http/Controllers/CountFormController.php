b6c4420dfb13a8fad936017ea258741942fa0768
Rohit <rohit.george@atree.org>
2021-08-31 12:32:35 +0530

    added import and newFile methods

--- app/Http/Controllers/CountFormController.php
+++ app/Http/Controllers/CountFormController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FormRow;
 use App\Models\CountForm;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class CountFormController extends Controller
 {
@@ -14,7 +16,90 @@ class CountFormController extends Controller
      */
     public function index()
     {
-        //
+        $form_nos = CountForm::count();
+
+        return view('butterfly_count.index', compact("form_nos"));
+    }
+
+    public function import(Request $request)
+    {
+        $path = $request->file('count_file')->store('count_file');
+        $original_name = $request->count_file->getClientOriginalName();
+        $raw_file = Excel::toArray(new CountForm, $path);
+        $form_id = $this->newFile($path, $original_name, $raw_file);
+        $form = CountForm::where("id", $form_id)->with("rows")->get();
+
+        return view('butterfly_count.validate', compact('form'));
+    }
+
+    public function newFile($file, $original_filename, $spreadsheet)
+    {
+        $formFields = [
+            ['name', 'Name of the Person taking the count'],
+            ['affilation', 'Affiliation (NGO/school, etc.)'],
+            ['phone', 'Contact Number'],
+            ['email', 'E Mail ID'],
+            ['team_members', 'Name and/or number of team members, if any'],
+            ['photo_link', 'Link to photo records uploaded'],
+            ['location', 'Location (Location name, nearest village/town, state)'],
+            ['coordinates', 'GPS Coordinates (from phone)'],
+            ['date', 'Date, start time and end time'],
+            ['altitude', 'Altitude, m'],
+            ['distance', 'Total distance covered on trail, km (approx. estimated)'],
+            ['weather', 'Weather (sunny, clouded, windy, etc.)'],
+            ['comments', 'Comments'],
+        ];
+        $rowFields = [
+            [ 'sl_no', 'Sl #'],
+            [ 'common_name', 'Common Name'],
+            [ 'scientific_name', 'Binomial Name (if known)'],
+            [ 'no_of_individuals', 'No. of individuals'],
+            [ 'remarks', 'Remarks (Male/Female/seasonal form, etc.)']
+        ];
+        foreach ($spreadsheet as $sheet) {
+            if (isset($sheet[11][2])) {
+                $form = new CountForm();
+                $form->file = $file;
+                $form->original_filename = $original_filename;
+                $count_row = -1;
+                foreach ($sheet as $k => $f) {
+                    foreach ($formFields as $ff) {
+                        if (str_replace("\n", " ", $sheet[$k][0]) == $ff[1]) {
+                            $form->{$ff[0]} = $sheet[$k][2];
+                        }
+                    }
+                    if (str_replace("\n", " ", $sheet[$k][0]) == $rowFields[0][1]) {
+                        $count_row = $k;
+                    }
+                }
+                $form->save();
+                for ($i = $count_row +1 ; $i < count($sheet); $i++) {
+                    $nullFlag = false;
+                    foreach ($sheet[$i] as $k=>$ele) {
+                        if ($ele != null and $k>0) {
+                            $nullFlag = true;
+                            break;
+                        }
+                    }
+                    if ($nullFlag) {
+                        $form_row = new FormRow();
+                        $form_row->count_form_id = $form->id;
+                        foreach ($rowFields as $k => $rf) {
+                            if ($k == 1) {
+                                $form_row->{$rf[0]} = trim(ucwords(strtolower($sheet[$i][$k])));
+                            } elseif ($k == 2) {
+                                $form_row->{$rf[0]} = trim(ucfirst(strtolower($sheet[$i][$k])));
+                            } else {
+                                $form_row->{$rf[0]} = trim($sheet[$i][$k]);
+                            }
+                        }
+                        $form_row->save();
+                    } else {
+                    }
+                }
+            }
+        }
+        return $form->id;
     }
 
     /**
