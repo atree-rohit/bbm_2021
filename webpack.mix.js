1afff918cee0d44a5e910c2928850834d7b20d2f
Rohit <rohit.george@atree.org>
2021-08-31 17:12:01 +0530

    css tweaks and added components to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/validate.js', 'public/js')
+    .js('resources/js/butterfly_counts.js', 'public/js')
     .vue()
     .css('resources/css/style.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
