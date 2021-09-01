04037c00fa086bfca3400914a40c3ebb59cb7639
atree-rohit <rohit.george@atree.org>
2021-09-01 07:40:14 +0530

    return card stats for homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CountForm;
+use App\Models\FormRow;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -11,18 +13,28 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
     public function index()
     {
-        return view('home');
+        $forms = CountForm::count();
+        $form_rows = FormRow::all();
+        $species = count($form_rows->groupBy("scientific_name"));
+        $individuals = $form_rows->sum("no_of_individuals_cleaned");
+        $data = [
+            "Forms" => [$forms, "bg-warning"],
+            "Species" => [$species, "bg-success"],
+            "Individuals" => [$individuals, "table-secondary"]
+        ];
+        return view('welcome', compact("data"));
     }
 }
