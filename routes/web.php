ab5beae2db6a77e501b2b945ad3c3410c0d6f3f1
Rohit <rohit.george@atree.org>
2021-09-03 16:48:02 +0530

    added route where pwa data will be posted

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Auth::routes();
 
 Route::post('/butterfly_count/import', [CountFormController::class, 'import']);
 Route::post('/butterfly_count/validate', [CountFormController::class, 'validate_form']);
+Route::post('/butterfly_count/pwa_post', [CountFormController::class, 'pwa_post']);
+
 Route::resource('/butterfly_count', CountFormController::class);
 // Route::resource('/species', FormRowController::class);
 Route::resource('/species', SpeciesController::class);
