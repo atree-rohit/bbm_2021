48fdff8a5c1b5f37579af2b001f86ef0934d5a98
atree-rohit <rohit.george@atree.org>
2021-09-05 15:55:24 +0530

    forms.blade and welcome-india added

---
+++ resources/views/welcome-india.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="card-group text-center">
+        @foreach($data as $k=>$d)
+        <div class="card color-overlay">
+            <div class="card-body color-{{$k}}">
+                <div class="card-text display-1">{{$d[0]}}</div>
+                <div class="card-title h1">{{$d[1]}}</div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    <div class="container-fluid border-bottom border-primary my-2 py-2"> </div>
+    <div class="container-fluid py-5 d-flex justify-content-center">
+        <a class="btn btn-outline-success btn-block w-75" id="app-button" href="/butterfly_count">Butterfly Count Form & App</a>
+    </div>
+</div>
+@endsection
