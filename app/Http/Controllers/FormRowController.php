6c529ed4f03905930ffa993b24e0482ccf2ba8cc
atree-rohit <rohit.george@atree.org>
2021-09-05 15:46:27 +0530

    method set_flag

--- app/Http/Controllers/FormRowController.php
+++ app/Http/Controllers/FormRowController.php
@@ -17,6 +17,15 @@ class FormRowController extends Controller
         //
     }
 
+
+    public function set_flag(Request $request){
+        $row = FormRow::find($request["id"]);
+        $row->flag = $request["flag"];
+        $row->save();
+
+        return response()->json("success", 200);
+    }
+
     /**
      * Show the form for creating a new resource.
      *
