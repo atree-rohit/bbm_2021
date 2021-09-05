4c8f7f58256136352ee8f3a95350792bd1b2ad95
atree-rohit <rohit.george@atree.org>
2021-09-05 15:53:34 +0530

    stats ignores flags and duplicate; set sci_name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,20 +26,61 @@ class HomeController extends Controller
 
     public function welcome()
     {
-        $forms = CountForm::count();
-        $form_rows = FormRow::all();
-        $species = count($form_rows->groupBy("common_name"));
-        // dd($form_rows->groupBy("scientific_name"));
-        $individuals = 0;
-        foreach($form_rows as $f){
-            if(is_numeric($f->individuals))
-                $individuals += $f->individuals;
-        }
+        $forms = CountForm::where("flag", false)->count();
+        $form_rows = FormRow::with("form")->get();
+        $stats = $this->populate_sci_names($form_rows);
+        
         $data = [
             [$forms,"Forms", "bg-warning"],
-            [$species,"Species", "bg-success"],
-            [$individuals,"Individuals", "bg-indigo"]
+            [count($stats[0]),"Species", "bg-success"],
+            [$stats[1],"Individuals", "bg-indigo"]
         ];
-        return view('welcome', compact("data"));
+        // if(isset($_GET["new"]))
+            return view('welcome-india', compact("data"));
+        // return view('welcome', compact("data"));
+    }
+
+    public function populate_sci_names($rows){
+        $names = [
+            "synoptic" => json_decode(file_get_contents(public_path('/data/synoptic.json'))),
+            "boi" => json_decode(file_get_contents(public_path('/data/boi.json'))),
+        ];
+        $scientific_names = [];
+        $individuals = 0;
+        $skipped = $sci_name_not_set = [];
+        foreach($rows as $row){
+            //ignore if row is flag, form is flag or duplicate
+            if(!($row->flag || $row->form->flag || $row->form->duplicate)){
+                if(is_numeric($row->individuals))
+                    $individuals += $row->individuals;
+                if(!$row->scientific_name){
+                    $sci_name_flag = true;
+                    foreach($names as $list_name=>$list){
+                        foreach($list as $sp){
+                            if($sp[1] == $row->common_name){
+                                $sci_name_flag = false;
+                                $row->scientific_name = $sp[0];
+                                $row->save();
+                                if(!in_array($sp[0], $scientific_names))
+                                    $scientific_names[] = $sp[0];
+                                break;
+                            }
+                        }
+
+                    }
+                    if($sci_name_flag){
+                        $sci_name_not_set[] = $row;
+                    }
+                } else {
+                    if(!in_array($row->scientific_name, $scientific_names))
+                        $scientific_names[] = $row->scientific_name;
+                }
+            } else {
+                $skipped[] = [$row->id, $row->flag, $row->form->flag, $row->form->duplicate];
+            }
+        }
+        // dd($skipped, $sci_name_not_set);
+        return ([$scientific_names, $individuals]);
+        
     }
 }
