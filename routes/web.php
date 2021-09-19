0fd1526c162a1815ccdc01bf9ead971efc247750
atree-rohit <rohit.george@atree.org>
2021-09-19 18:47:26 +0530

    updated calendar route; added middleware on store_taxon and pull

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::post('/butterfly_count/validate', [CountFormController::class, 'validate_
 Route::post('/butterfly_count/pwa_post', [CountFormController::class, 'pwa_post']);
 Route::get('/butterfly_count/pwa_app', [CountFormController::class, 'pwa_app']);
 Route::get('/butterfly_count/forms', [CountFormController::class, 'forms'])->middleware('auth');
-Route::get('/butterfly_count/calendar', [CountFormController::class, 'calendar']);
+Route::get('/calendar', [CountFormController::class, 'calendar']);
 Route::post('/butterfly_count/set_flag', [CountFormController::class, 'set_flag'])->middleware('auth');
 Route::post('/butterfly_count/set_flag', [CountFormController::class, 'set_flag'])->middleware('auth');
 Route::post('/butterfly_count/set_duplicate', [CountFormController::class, 'set_duplicate'])->middleware('auth');
@@ -40,8 +40,8 @@ Route::post('/butterfly_count/set_row_flag', [FormRowController::class, 'set_fla
 
 Route::post('/inat/store_observation', [INatController::class, 'store_observation'])->name('inat.store_observation');
 Route::post('/inat/update_observation', [INatController::class, 'update_observation'])->name('inat.update_observation');
-Route::post('/inat/store_taxon', [INatController::class, 'store_taxon'])->name('inat.store_taxon');
-Route::get('/inat/pull', [INatController::class, 'pull'])->name('inat.pull');
+Route::post('/inat/store_taxon', [INatController::class, 'store_taxon'])->name('inat.store_taxon')->middleware('auth');;
+Route::get('/inat/pull', [INatController::class, 'pull'])->name('inat.pull')->middleware('auth');;
 
 
 Route::resource('/butterfly_count', CountFormController::class);
