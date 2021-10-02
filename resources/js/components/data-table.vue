1c7fbc4670aa21e803db441cff2e143f636c00c2
atree-rohit <rohit.george@atree.org>
2021-10-02 21:46:18 +0530

    added emit row id; internal overflow-x

--- resources/js/components/data-table.vue
+++ resources/js/components/data-table.vue
@@ -1,6 +1,7 @@
 <style lang="css" scoped>
     .table-container{
         width:100%;
+        max-width:50vw;
         height: 60vh;
         overflow: auto;
     }
@@ -18,18 +19,27 @@
         white-space: nowrap;
         background: #ccf;
     }
+    .tableFixHead tbody tr:hover{
+        background: #ffa;
+        cursor: pointer;
+    }
+    @media screen and (max-width: 800px) {
+        .table-container{
+            max-width:95vw;
+        }
+    }
 </style>
 
 <template>
     <div class="table-container">
-        <table class="table tableFixHead all-states-table">
+        <table class="table tableFixHead">
             <thead class="">
                 <tr>
                     <th v-for="h in headers" :key="h[1]" v-text="h[0]"></th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(row, id) in data" :key="id">
+                <tr v-for="(row, id) in data" :key="id" @click="$emit('rowClick', id)">
                     <td v-for="h in headers"
                         :key="h[1]"
                         v-text="row[h[1]]"
