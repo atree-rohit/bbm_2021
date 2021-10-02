b84d7eaf7d51eb82c17a41be56f493f9ce5bcc76
atree-rohit <rohit.george@atree.org>
2021-10-02 20:04:38 +0530

    made all function golbal and many variables also

--- resources/js/components/india-map.vue
+++ resources/js/components/india-map.vue
@@ -1,7 +1,8 @@
 <style>
 	#map-container .selected{
-		fill: #afa;
-		stroke: rgba(10,100,10,.75);
+		/*fill: #afa;*/
+		fill: #ff5;
+		stroke: rgba(255,50,0,.5);
 		stroke-width:.5px;
 	}
 
@@ -12,12 +13,19 @@
 </template>
 
 <script>
+import * as d3Legend from "d3-svg-legend"
 import country from '../country.json'
 export default {
 	name:"india-map",
 	props: ["map_data", "selected_state", "popup", "stateStats"],
 	data() {
 		return{
+			states: null,
+			path: null,
+			svg: {},
+			projection: null,
+
+
 			state_data: {},
 			state_max: 0,
 			height: window.innerHeight * 0.8,
@@ -28,6 +36,7 @@ export default {
 	},
 	mounted(){
 		this.init()
+		// alert(`${this.width} x ${this.height}`)
 	},
 	computed:{
 		stateData () {
@@ -53,17 +62,48 @@ export default {
 				})
 			}
 			return op
-		}
+		},
+		zoom() {
+			let that = this
+			return d3.zoom()
+					.scaleExtent([.5, 50])
+					.translateExtent([[-0.5 * this.width,-0.75 * this.height],[2.5 * this.width, 2.5 * this.height]])
+					.on('zoom', function() {
+
+						that.svg.selectAll('.poly_text')
+							.attr('transform', d3.event.transform),
+						that.svg.selectAll('path')
+							.attr('transform', d3.event.transform),
+						that.svg.selectAll('circle')
+							.attr('transform', d3.event.transform)
+						.attr("r", 2 / d3.event.transform.k)
+					})
+		},
 	},
 	watch: {
 		map_data () {
 			this.init()
 			// this.renderMap()
 		},
+		selected_state (newVal, oldVal) {
+			if (!d3.select("#map-container .map-points").empty()) {
+				d3.selectAll(".map-points").remove()
+			}
+			if(newVal != 'All')
+				this.mapPoints()
+		}
 	},
 	methods:{
 		init () {
 			this.map_first_render = true
+			this.states = null
+			this.path = null
+			this.svg = {}
+			this.projection = null
+
+
+			this.state_data = {}
+			this.state_max = 0
 			country.features.forEach(s => {
 				this.state_data[s.properties.ST_NM] = [];
 			})
@@ -72,7 +112,7 @@ export default {
 				if(Object.keys(this.state_data).indexOf(o.state) != -1){
 					this.state_data[o.state].push(o)
 				} else {
-					// console.log("unmatched state name", o.state, o)
+					console.log("unmatched state name", o.state, o)
 				}
 			});
 
@@ -84,45 +124,61 @@ export default {
 			this.renderMap()
 		},
 		renderMap () {
-			let that = this
-			let height = this.height
-			let width = this.width
-			if(height > width){
-				height /= 1.7
-				width *= 1.9
+			if(this.height > this.width){
+				this.height /= 1.7
+				this.width *= 1.9
 			}
 
 			if (!d3.select("#map-container svg").empty()) {
 				d3.selectAll("#map-container svg").remove()
 			}
-			let svg = d3.select("#map-container")
+			this.svg = d3.select("#map-container")
 						.append("svg")
 							.attr("preserveAspectRatio", "xMinYMin meet")
-							.attr("width", width)
-							.attr("height", height)
+							.attr("width", this.width)
+							.attr("height", this.height)
 							.style("background-color", "rgb(190, 229, 235)")
 							.classed("svg-content d-flex m-auto", true)
 
-			var projection = d3.geoMercator().scale(850).center([87, 25.5])
-			const path = d3.geoPath().projection(projection)
-			const colors = d3.scaleLinear().domain([0, 1, this.state_max]).range(["#f77", "#6a8", "#7f9"])
-			var legend = d3Legend.legendColor().scale(colors).shapeWidth(55).labelFormat(d3.format(".0f")).orient('horizontal').cells(6)
-			let base = svg.append("g")
+			this.projection = d3.geoMercator().scale(850).center([87, 25.5])
+			this.path = d3.geoPath().projection(this.projection)
+			const colors = d3.scaleLinear()
+							.domain([0, 1, this.state_max*.5, this.state_max])
+							.range(["#f77","#696", "#8c8", "#9f9"])
+			let legend = d3Legend.legendColor()
+								.scale(colors)
+								.labelFormat(d3.format(".0f"))
+								.orient('horizontal')
+								.labelOffset(-10)
+								.labelAlign("start")
+								.shapeWidth(45)
+								.cells(5)
+								.shapePadding(47)
+
+			if(this.height > this.width){
+				legend.shapeWidth(35)
+				.cells(4)
+				.shapePadding(37)
+			}
+
+
+			let base = this.svg.append("g")
 			.classed("map-boundary", true)
 
 			let base_text = base.selectAll("text").append("g")
 			base = base.selectAll("path").append("g")
-			let states = base.append("g").classed("states", true)
+			this.states = base.append("g").classed("states", true)
+			let that = this
 
 			country.features.forEach(state=> {
 				let s_name = state.properties.ST_NM
 				let that = this
 
-				let current_state = states.append("g")
+				let current_state = this.states.append("g")
 				.data([state])
 				.enter().append("path")
-				.attr("d", path)
-				.attr("id", s_name.replaceAll(" ", "_").replaceAll("&", ""))
+				.attr("d", this.path)
+				.attr("id", this.stateID(s_name))
 				.attr("title", s_name)
 				.on('mouseover', function (d, i) {
 					that.tooltip.html(
@@ -140,109 +196,98 @@ export default {
 						.style('left', d3.event.pageX + 10 + 'px');
 					})
 					.on('mouseout', () => that.tooltip.html(``).style('visibility', 'hidden'))
-					.on("click", clicked);
+					.on("click", this.clicked);
 
 					if(this.stateData[s_name] == undefined){
 						current_state.attr("fill", (d) => colors(-1))
 					} else if (s_name == this.selected_state) {
 						current_state.classed("selected", true)
 					} else {
-						current_state.attr("fill", (d) => colors(this.stateData[s_name].length))
+						current_state.attr("fill", (d) => colors(this.stateData[s_name].length)).text("FUCK")
 					}
 			})
 
-			let zoom = d3.zoom()
-			.scaleExtent([.5, 50])
-			.translateExtent([[-0.5 * width,-0.75 * height],[2.5 * width, 2.5 * height]])
-			.on('zoom', function() {
-				svg.selectAll('.poly_text')
-				.attr('transform', d3.event.transform),
-				svg.selectAll('path')
-				.attr('transform', d3.event.transform),
-				svg.selectAll('circle')
-				.attr('transform', d3.event.transform)
-				.attr("r", 2 / d3.event.transform.k);
-			});
-			svg.call(zoom);
+			this.svg.append("g")
+				.attr("transform", "translate("+this.width*.5+", 50)")
+				.call(legend)
+				// .append("text")
+				// .classed("map_label", true)
+				// .attr("dx", 5)
+				// .attr("dy", -10)
+				// .classed("h1", true)
+				// .text(this.selected_state)
+
+			this.svg.call(this.zoom)
 
 			if(this.map_first_render){
-				// console.log(this.selected_state)
-				map_init(this.selectedGeoJson)
+				this.clicked(this.selectedGeoJson)
 				this.map_first_render = false
-				mapPoints()
+				
 			}
-			function map_init(d) {
-				let state = d.properties.ST_NM
-				let [[x0, y0], [x1, y1]] = [[0,0],[0,0]]
-				states.transition().style("fill", null);
-				if(that.selected_state != 'All'){
-					d3.select("#" + that.selected_state.replaceAll(" ", "_").replaceAll("&", "")).transition().style("fill", null);
-				}
-				if(that.selected_state == 'All'){
-					[[x0, y0], [x1, y1]] = path.bounds(country)
+		},
+		stateID(s){
+			return s.replaceAll(" ", "_").replaceAll("&", "")
+		},
+		clicked(d) {
+			this.tooltip.html(``).style('visibility', 'hidden')
+			let state = d.properties.ST_NM
+			let [[x0, y0], [x1, y1]] = [[0,0],[0,0]]
+
+			this.states.transition().style("fill", null)
+
+			if(d3.select(".selected")["_groups"][0][0] != null){
+				d3.select("#" + d3.select(".selected")["_groups"][0][0].id).attr("class", null)
+				
+			}
+			
+			if(this.selected_state != 'All'){
+			}
+			if(this.selected_state == state){
+				[[x0, y0], [x1, y1]] = this.path.bounds(country)
+			} else {
+				[[x0, y0], [x1, y1]] = this.path.bounds(d)
+				d3.select("#" + this.stateID(state)).classed("selected", true)
+				
+			}
+			if(!this.map_first_render){
+				if(this.selected_state == state){
+					this.$emit('stateSelected', 'All')
 				} else {
-					[[x0, y0], [x1, y1]] = path.bounds(d)
-					d3.select(this).transition().style("fill", "gold")
+					this.$emit('stateSelected', state)
 				}
-
-				svg.transition().duration(751).call(
-					zoom.transform,
-					d3.zoomIdentity
-					.translate(width / 2, height / 2)
-					.scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-					.translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-				);
 			}
+			
+			this.svg.transition().duration(750).call(
+				this.zoom.transform,
+				d3.zoomIdentity
+				.translate(this.width / 2, this.height / 2)
+				.scale(Math.min(8, 0.9 / Math.max((x1 - x0) / this.width, (y1 - y0) / this.height)))
+				.translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+			)
+			/*
+			*/
+		},
+		mapPoints(){
+			let points = []
 
-			function clicked(d) {
-				that.tooltip.html(``).style('visibility', 'hidden')
-				let state = d.properties.ST_NM
-				let [[x0, y0], [x1, y1]] = [[0,0],[0,0]]
-				states.transition().style("fill", null);
-				if(that.selected_state != 'All'){
-					d3.select("#" + that.selected_state.replaceAll(" ", "_").replaceAll("&", "")).transition().style("fill", null);
-				}
-				if(that.selected_state == state){
-					[[x0, y0], [x1, y1]] = path.bounds(country)
-					that.$emit('stateSelected', 'All')
-				} else {
-					[[x0, y0], [x1, y1]] = path.bounds(d);
-					that.$emit('stateSelected', state)
-					d3.select(this).transition().style("fill", "gold");
-				}
-				svg.transition().duration(750).call(
-					zoom.transform,
-					d3.zoomIdentity
-					.translate(width / 2, height / 2)
-					.scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-					.translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-				);
+			if(this.selected_state != 'All'){
+				this.state_data[this.selected_state].forEach(o => {
+					let coords = o.location.split(",")
+					points.push([coords[1], coords[0], o.id, o.place_guess]);
+				})
 			}
+			
+			if(points.length > 0){
+				let map_points = this.svg.append('g')
+				.classed('map-points', true)
+				.selectAll("circle")
+				.data(points).enter()
+				.append("circle")
+				.attr("cx", (d) => this.projection(d)[0])
+				.attr("cy", (d) => this.projection(d)[1])
+				.attr("r", "0px")
 
-			function mapPoints(){
-				if (!d3.select("#map-container .map-points").empty()) {
-					d3.selectAll(".map-points").remove()
-				}
-				let points = [];
-				if(that.selected_state != 'All'){
-					// console.log(that.selected_state);
-					that.state_data[that.selected_state].forEach(o => {
-						let coords = o.location.split(",")
-						points.push([coords[1], coords[0], o.id, o.place_guess]);
-					})
-				}
-				if(points.length > 0){
-					let map_points = svg.append('g')
-					.classed('map-points', true)
-					.selectAll("circle")
-					.data(points).enter()
-					.append("circle")
-					.attr("cx", (d) => projection(d)[0])
-					.attr("cy", (d) => projection(d)[1])
-					.attr("r", "0px")
-
-					// map_points.on("click", (d) => that.setMissingState(d))
-				}
+				// map_points.on("click", (d) => that.setMissingState(d))
 			}
 		},
 	}
