97fd5ce366bb1074e658da774a24ff5ebf654ec1
atree-rohit <rohit.george@atree.org>
2021-10-06 19:59:46 +0530

    select and deselect rows, header color and scrollbar

--- resources/js/components/data-table.vue
+++ resources/js/components/data-table.vue
@@ -4,10 +4,26 @@
         overflow-y: scroll;
         overflow-x: auto;
     }
-    /* .table-container .tableFixHead{
-        height:100%;
-        overflow: scroll;
-    } */
+    
+    #table-container::-webkit-scrollbar-track
+    {
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+        border-radius: 10px;
+        background-color: #F5F5F5;
+    }
+
+    #table-container::-webkit-scrollbar
+    {
+        width: 7px;
+        background-color: #F5F5F5;
+    }
+
+    #table-container::-webkit-scrollbar-thumb
+    {
+        border-radius: 10px;
+        -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+        background-color: #a7c;
+    }
     .tableFixHead{
         /* overflow: auto; */
         height: 100px;
@@ -20,7 +36,7 @@
         top: 0;
         z-index: 1;
         white-space: nowrap;
-        background: #ccf;
+        background: #cad;
     }
     .tableFixHead tbody tr:hover{
         background: #ffa;
@@ -48,7 +64,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(row, id) in data" :key="id" @click="$emit('rowClick', id)">
+                    <tr v-for="(row, id) in data"
+                        :key="id"
+                        :class="selected_rows.indexOf(id) != -1? 'bg-success':''"
+                        @click="$emit('rowClick', id)"
+                    >
                         <td v-for="h in headers"
                             :key="h[1]"
                             v-text="row[h[1]]"
@@ -63,13 +83,24 @@
 <script>
 export default {
     name: "data-table",
-    props: ["data", "headers"],
+    props: ["data", "headers", "selected", "selected_col"],
+
     mounted() {
         this.updateHeight()
     },
     data(){
         return {
-            height: 0
+            height: 0,
+            selected_rows:[],
+        }
+    },
+    watch:{
+        selected (newVal) {
+            this.selected_rows = []
+            this.data.forEach((r,id) => {
+                if(newVal.indexOf(r[this.selected_col]) != -1)
+                    this.selected_rows.push(id)
+            })
         }
     },
     computed: {
