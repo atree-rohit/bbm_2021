7b873a53b7f8311c1835e79877cd0c0a9b6740c8
atree-rohit <rohit.george@atree.org>
2021-10-07 07:09:42 +0530

    added place guess for observations overlay card

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -15,18 +15,22 @@ class ResultController extends Controller
         $last_update = strtotime(iNat::latest('updated_at')->first()->updated_at) + 5.5*60*60;
         $last_update = date("d M Y, h:i:s A", $last_update);
 
-        $inat_taxa = iNatTaxa::limit(-1)->get()->keyBy("id");
+        $inat_taxa = iNatTaxa::select("id", "name", "common_name", "rank", "ancestry")
+                                ->limit(-1)
+                                ->get()
+                                ->keyBy("id");
         
         $inat_data = iNat::select("id", "uuid", "observed_on", "location", "place_guess", "state", "taxa_id", "taxa_name", "taxa_rank", "img_url", "user_id", "user_name", "quality_grade", "license_code", "inat_created_at")
-                        // ->limit(100)
+                        // ->limit(1000)
                         ->get()
                         ->toArray();
         $forms = CountForm::where("flag", 0)
-                        // ->limit(10)
+                        // ->limit(50)
                         ->with("rows")
                         ->get()
                         ->toArray();
         
+        $inat_unset_fields = ["uuid", "observed_on", "quality_grade", "license_code", "inat_created_at"];
         $form_data = [];
 
         foreach ($inat_data as $k=>$id) {
@@ -34,31 +38,10 @@ class ResultController extends Controller
             $inat_data[$k]["created_at"] = (int) date('d', strtotime('+5 hours +30 minutes', $timestamp));
             $inat_data[$k]["portal"] = "inat";
             $inat_data[$k]["individuals"] = 1;
+            foreach($inat_unset_fields as $iuf){
+                unset($inat_data[$k][$iuf]);
+            }
         }
-
-        // this.form_data.forEach(f => {
-        //             let form_details = {
-        //                 user_id: f.name,
-        //                 user_name: f.name,
-        //                 state: f.state,
-        //                 location: `${f.latitude},${f.longitude}`,
-        //                 created_at: parseInt(f.date_cleaned.split("-")[0]) ?? null,
-        //                 img_url: "",
-        //                 portal: "counts"
-        //             }
-        //             f.rows.forEach(s => {
-        //                 if(s.flag == 0){
-        //                     let row = form_details
-        //                     row.id = s.id
-        //                     row.taxa_id = s.inat_taxa_id
-        //                     row.taxa_name = s.scientific_name_cleaned
-        //                     row.taxa_rank = s.id_quality
-        //                     row.individuals = s.no_of_individuals_cleaned
-
-        //                     this.all_data.push(row)
-        //                 }
-        //             })
-        //         })
         foreach ($forms as $f) {
             $created = explode("-", $f["date_cleaned"]);
             $d = [
