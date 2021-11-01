b5a93107d6e834b137b0c3db97fb402c9d984f36
Rohit <rohit.george@atree.org>
2021-11-01 14:48:51 +0530

    boi migration

---
+++ database/migrations/2021_11_01_065151_create_b_o_i_s_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBOISTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('b_o_i_s', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('boi_id')->nullable();
+            $table->string('created_date')->nullable();
+            $table->text('observed_date')->nullable();
+            $table->string('media_code')->nullable();
+            $table->string('species_name')->nullable();
+            $table->string('user')->nullable();
+            $table->string('life_stage')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('district')->nullable();
+            $table->string('location_name')->nullable();
+            $table->string('latitude')->nullable();
+            $table->bigInteger('inat_taxa_id')->nullable();
+            $table->string('longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('b_o_i_s');
+    }
+}
