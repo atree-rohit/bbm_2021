9cea08dc3e10624f7ec4d5c7d89850e70adb5d06
atree-rohit <rohit.george@atree.org>
2021-11-08 06:24:21 +0530

    boi data cleaning methods

--- app/Http/Controllers/BOIController.php
+++ app/Http/Controllers/BOIController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BOI;
+use App\Models\iNat;
 use App\Models\iNatTaxa;
 use Illuminate\Http\Request;
 
@@ -15,9 +16,30 @@ class BOIController extends Controller
      */
     public function index()
     {
-        $data = BOI::all();
+        $data = BOI::where("flag", false)
+                    ->where("created_date", "like", "%/09/21%")
+                    ->where("observed_date", "like", "%/09/21%")
+                    ->where("country", "like", "%India%")
+                    ->get();
+
         $not_bbm = $bbm = $bbm_no_taxa = $unmatched_taxa = [];
         $fields = ["boi_id", "created_date", "observed_date", "media_code", "species_name", "user", "life_stage", "country", "state", "district", "location_name", "latitude", "longitude", "inat_taxa_id"];
+
+        $geojson = json_decode(file_get_contents(public_path('data/country.geojson')));
+        $geojson_states = [];
+        foreach ($geojson->features as $state) {
+            $geojson_states[] = $state->properties->ST_NM;
+        }
+        
+        foreach($data->groupBy("state") as $s=>$obvs){
+            if(!in_array($s, $geojson_states))
+                echo "$s<br>";
+        }
+        dd($geojson_states);
+
+
+
+        /*
         foreach($data as $d){
             $exclude_flag = false;
             if(!strpos($d->created_date, '/09/21')){
@@ -50,18 +72,22 @@ class BOIController extends Controller
             echo "<th>".$f."</th>";
         }
         echo "</tr>";
-        foreach($bbm_no_taxa as $b){
-            echo "<tr>";
-            foreach($fields as $f){
-                echo "<td>".$b->{$f}."</td>";
+        foreach($data as $b){
+            if($b->inat_taxa_id == null){
+                echo "<tr>";
+                foreach($fields as $f){
+                    echo "<td>".$b->{$f}."</td>";
+                }
+                echo "</tr>";                
             }
-            echo "</tr>";
         }
         echo "</table>";
         ksort($unmatched_taxa);
         
         // dd(count(array_unique(array_column($not_bbm, 'boi_id'))));
-        dd($unmatched_taxa);
+        // dd($unmatched_taxa);
+        dd($not_bbm);
+        */
 
     }
 
@@ -125,6 +151,83 @@ class BOIController extends Controller
         //
     }
 
+    public function fix_taxa0(){
+        $unmatched_taxa = BOI::where("inat_taxa_id", null)->where("flag", 0)->get()->groupBy("species_name");
+        $inat_taxa = iNatTaxa::get()->keyBy("name");
+        $count = 0;
+        $fix_genera = [
+                        "Charaxes" => "Polyura",
+                        "Spindasis" => "Cigaritis",
+                        "Burara" => "Bibasis",
+                        "Symphaedra" => "Euthalia",
+                        "Chilades" => "Luthrodes",
+                        "Everes" => "Cupido"
+                    ];
+
+        /*
+        foreach($unmatched_taxa as $s => $ut){
+            if(isset($inat_taxa[$s])){
+                foreach($ut as $o){
+                    $o->inat_taxa_id = $inat_taxa[$s]->id;
+                    $o->save();
+                    $count++;
+                }
+            }
+        }
+        */
+        foreach($unmatched_taxa as $s => $ut){
+            $species_split = explode(" ", $s);
+            $genus = $species_split[0];
+            if(isset($fix_genera[$genus])){
+                $fiexd_name = $fix_genera[$genus] . " " . $species_split[1];
+                if(isset($inat_taxa[$fiexd_name])){
+                    foreach($ut as $o){
+                        $o->inat_taxa_id = $inat_taxa[$fiexd_name]->id;
+                        // dd($o);
+                        $o->save();
+                        $count++;
+                    }
+                }
+            }
+            
+        }
+
+        $x = array_keys($unmatched_taxa->toArray());
+        sort($x);
+        dd($x);
+
+        dd($unmatched_taxa, $inat_taxa, $count);
+    }
+    public function fix_taxa()
+    {
+        $inat_data = iNat::select("id", "taxa_id")->get();
+        $all_taxa = iNat::select("taxa_id")->distinct()->get()->pluck("taxa_id")->toArray();
+        $taxa = iNatTaxa::all();
+        $saved_taxa = $ancestors = [];
+
+        $taxa_to_pull = [358633, 1148666];
+
+        array_multisort($all_taxa);
+        if (($key = array_search(null, $all_taxa)) !== false) {
+            unset($all_taxa[$key]);
+        }
+        foreach ($taxa as $t) {
+            $saved_taxa[] = $t->id;
+            $ancestry = explode("/", $t->ancestry);
+            foreach ($ancestry as $a) {
+                if (($a != "") && (!in_array($a, $ancestors))) {
+                    $ancestors[] = (int) $a;
+                }
+            }
+        }
+        array_multisort($ancestors);
+        if (($key = array_search(null, $ancestors)) !== false) {
+            unset($ancestors[$key]);
+        }
+        // dd($all_taxa, $saved_taxa, $ancestors);
+
+        return view("boi.fix_taxa", compact("all_taxa", "saved_taxa", "ancestors", "taxa_to_pull"));
+    }
     /**
      * Display the specified resource.
      *
