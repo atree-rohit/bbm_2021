7c59cb9c0c2e14970c474f70359ca99fa33d64ea
atree-rohit <rohit.george@atree.org>
2021-11-09 19:20:37 +0530

    commented out cleaning routes

--- routes/web.php
+++ routes/web.php
@@ -54,12 +54,14 @@ Route::post('/inat/store_taxon', [INatController::class, 'store_taxon'])->name('
 Route::get('/inat/pull', [INatController::class, 'pull'])->name('inat.pull')->middleware('auth');
 
 Route::get('/result', [ResultController::class, 'index'])->name('result.index');
-Route::get('/ibp/missing_taxa', [IBPController::class, 'missingTaxa'])->middleware('auth');
 
+/*
+Route::get('/ibp/missing_taxa', [IBPController::class, 'missingTaxa'])->middleware('auth');
 Route::get('/boi/fix_taxa', [BOIController::class, 'fix_taxa'])->middleware('auth');
 
 Route::resource('/butterfly_count', CountFormController::class);
-// Route::resource('/inat', INatController::class);
 Route::resource('/ibp', IBPController::class)->middleware('auth');
 Route::resource('/boi', BOIController::class)->middleware('auth');
+*/
+// Route::resource('/inat', INatController::class);
 // Route::resource('/species', SpeciesController::class);
