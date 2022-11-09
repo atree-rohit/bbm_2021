ffece4306ac6bee52d3d8f009ba75ea33f1102b6
Rohit George <rohit.george@atree.org>
2022-11-09 17:05:16 +0530

    modified webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/result.js', 'public/js')
+    .js('resources/js/result_2022.js', 'public/js')
     .js('resources/js/validate.js', 'public/js')
     .js('resources/js/butterfly_counts.js', 'public/js')
     .js('resources/js/inat.js', 'public/js')
@@ -21,4 +22,5 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/boi_fix_taxa.js', 'public/js')
     .vue()
     .css('resources/css/style.css', 'public/css')
+    .css('resources/css/result_2022.css', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
